23f4530a2d601ae08d10709d667eb0ce1b678f91
liquidsword <audenrobertson@gmail.com>
2018-06-26 19:30:49 -0400

    created view files and layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "masterpiece_secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:artist_id]
+    end
+
+    def current_artist
+      Artist.find(session[:artist_id])
+    end
+
+  end
 end
